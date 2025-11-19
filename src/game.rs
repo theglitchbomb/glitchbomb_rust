55a093e8211fc00969e1286a09f344c0c3f67b4d
elton-cs <govada.elton@gmail.com>
2025-11-19 06:55:25 -0600

    feat: game state machine

--- src/game.rs
+++ src/game.rs
@@ -1,28 +1,52 @@
 enum GameState {
-    InGame(ActiveGameData),
-    Won(VictoryData),
-    Lost(DefeatData),
+    Level(LevelData),
+    LevelComplete(LevelCompleteData),
+    Shop(ShopData),
+    CashedOut(CashedOutData),
+    GameOver(GameOverData),
 }
 
-struct ActiveGameData;
-struct VictoryData;
-struct DefeatData;
+struct LevelData;
+struct ShopData;
+struct LevelCompleteData;
+struct CashedOutData;
+struct GameOverData;
 
 enum GameAction {
+    StartLevel,
+    EnterShop,
+    BuyOrb,
     PullOrb,
     CashOut,
 }
 
 enum PullExecution {
-    PullsBomb,
-    PullsNonBomb,
+    HealthReachesZero,
+    PointsReachMilestone,
+    OrbsRunOut,
+    Continues,
 }
 
 fn stf_game(state: GameState, action: &GameAction, execution: &PullExecution) -> GameState {
     match (state, action, execution) {
-        (GameState::InGame(data), GameAction::PullOrb, PullExecution::PullsBomb) => GameState::Lost(DefeatData),
-        (GameState::InGame(data), GameAction::PullOrb, PullExecution::PullsNonBomb) => GameState::InGame(data),
-        (GameState::InGame(data), GameAction::CashOut, _) => GameState::Won(VictoryData),
+        (GameState::Shop(data), GameAction::BuyOrb, _) => GameState::Shop(data),
+        (GameState::Shop(data), GameAction::StartLevel, _) => GameState::Level(LevelData),
+        (GameState::Level(data), GameAction::PullOrb, PullExecution::HealthReachesZero) => {
+            GameState::GameOver(GameOverData)
+        }
+        (GameState::Level(data), GameAction::PullOrb, PullExecution::OrbsRunOut) => {
+            GameState::GameOver(GameOverData)
+        }
+        (GameState::Level(data), GameAction::PullOrb, PullExecution::Continues) => {
+            GameState::Level(data)
+        }
+        (GameState::Level(data), GameAction::PullOrb, PullExecution::PointsReachMilestone) => {
+            GameState::LevelComplete(LevelCompleteData)
+        }
+        (GameState::LevelComplete(data), GameAction::EnterShop, _) => GameState::Shop(ShopData),
+        (GameState::LevelComplete(data), GameAction::CashOut, _) => {
+            GameState::CashedOut(CashedOutData)
+        }
         (state, _, _) => state,
     }
 }
