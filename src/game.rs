323ffadc542d97ee80d6054af7d35eae3d313897
elton-cs <govada.elton@gmail.com>
2025-11-19 06:30:01 -0600

    fix: remove orb type impl

--- src/game.rs
+++ src/game.rs
@@ -1,43 +1,27 @@
 enum GameState {
-    InGame(ActiveGame),
+    InGame(ActiveGameData),
     Won(VictoryData),
     Lost(DefeatData),
 }
 
-struct ActiveGame;
+struct ActiveGameData;
 struct VictoryData;
 struct DefeatData;
 
-enum OrbType {
-    PowerUp(PowerUpOrb),
-    Benefit(BenefitOrb),
-    Bomb(BombOrb),
-    Multiplier(MultiplierOrb),
-}
-
-struct PowerUpOrb;
-struct BenefitOrb;
-struct BombOrb;
-struct MultiplierOrb;
-
 enum GameAction {
     PullOrb,
-    StartGame,
-    EndTurn,
     CashOut,
 }
 
 enum PullExecution {
-    TriggersWin,
-    TriggersBomb,
-    Continues,
+    PullsBomb,
+    PullsNonBomb,
 }
 
 fn stf_game(state: GameState, action: &GameAction, execution: &PullExecution) -> GameState {
     match (state, action, execution) {
-        (GameState::InGame(data), GameAction::PullOrb, PullExecution::TriggersWin) => GameState::Won(VictoryData),
-        (GameState::InGame(data), GameAction::PullOrb, PullExecution::TriggersBomb) => GameState::Lost(DefeatData),
-        (GameState::InGame(data), GameAction::PullOrb, PullExecution::Continues) => GameState::InGame(data),
+        (GameState::InGame(data), GameAction::PullOrb, PullExecution::PullsBomb) => GameState::Lost(DefeatData),
+        (GameState::InGame(data), GameAction::PullOrb, PullExecution::PullsNonBomb) => GameState::InGame(data),
         (GameState::InGame(data), GameAction::CashOut, _) => GameState::Won(VictoryData),
         (state, _, _) => state,
     }
