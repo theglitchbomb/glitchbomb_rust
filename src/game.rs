7a6e8e24b496ab76fabbe29a938e4c176035424a
elton-cs <govada.elton@gmail.com>
2025-11-19 18:22:19 -0600

    wip: game sm

--- src/game.rs
+++ src/game.rs
@@ -13,40 +13,391 @@ struct CashedOutData;
 struct GameOverData;
 
 enum GameAction {
-    StartLevel,
-    EnterShop,
-    BuyOrb,
     PullOrb,
     CashOut,
+    EnterShop,
+    BuyOrb,
+    NextLevel,
 }
 
 enum PullEffect {
     HealthReachesZero,
-    PointsReachMilestone,
-    OrbsRunOut,
-    Continues,
+    PointsLtMilestone,
+    PointsGteMilestone,
+    OrbsReachZero,
 }
 
 fn stf_game(state: GameState, action: &GameAction, effect: &PullEffect) -> GameState {
     match (state, action, effect) {
-        (GameState::Shop(data), GameAction::BuyOrb, _) => GameState::Shop(data),
-        (GameState::Shop(data), GameAction::StartLevel, _) => GameState::Level(LevelData),
-        (GameState::Level(data), GameAction::PullOrb, PullEffect::HealthReachesZero) => {
-            GameState::GameOver(GameOverData)
+        (GameState::Level(level_data), GameAction::PullOrb, PullEffect::HealthReachesZero) => {
+            todo!()
         }
-        (GameState::Level(data), GameAction::PullOrb, PullEffect::OrbsRunOut) => {
-            GameState::GameOver(GameOverData)
+        (GameState::Level(level_data), GameAction::PullOrb, PullEffect::PointsLtMilestone) => {
+            todo!()
         }
-        (GameState::Level(data), GameAction::PullOrb, PullEffect::Continues) => {
-            GameState::Level(data)
+        (GameState::Level(level_data), GameAction::PullOrb, PullEffect::PointsGteMilestone) => {
+            todo!()
         }
-        (GameState::Level(data), GameAction::PullOrb, PullEffect::PointsReachMilestone) => {
-            GameState::LevelComplete(LevelCompleteData)
+        (GameState::Level(level_data), GameAction::PullOrb, PullEffect::OrbsReachZero) => todo!(),
+        (GameState::Level(level_data), GameAction::CashOut, PullEffect::HealthReachesZero) => {
+            todo!()
         }
-        (GameState::LevelComplete(data), GameAction::EnterShop, _) => GameState::Shop(ShopData),
-        (GameState::LevelComplete(data), GameAction::CashOut, _) => {
-            GameState::CashedOut(CashedOutData)
+        (GameState::Level(level_data), GameAction::CashOut, PullEffect::PointsLtMilestone) => {
+            todo!()
         }
-        (state, _, _) => state,
+        (GameState::Level(level_data), GameAction::CashOut, PullEffect::PointsGteMilestone) => {
+            todo!()
+        }
+        (GameState::Level(level_data), GameAction::CashOut, PullEffect::OrbsReachZero) => todo!(),
+        (GameState::Level(level_data), GameAction::EnterShop, PullEffect::HealthReachesZero) => {
+            todo!()
+        }
+        (GameState::Level(level_data), GameAction::EnterShop, PullEffect::PointsLtMilestone) => {
+            todo!()
+        }
+        (GameState::Level(level_data), GameAction::EnterShop, PullEffect::PointsGteMilestone) => {
+            todo!()
+        }
+        (GameState::Level(level_data), GameAction::EnterShop, PullEffect::OrbsReachZero) => todo!(),
+        (GameState::Level(level_data), GameAction::BuyOrb, PullEffect::HealthReachesZero) => {
+            todo!()
+        }
+        (GameState::Level(level_data), GameAction::BuyOrb, PullEffect::PointsLtMilestone) => {
+            todo!()
+        }
+        (GameState::Level(level_data), GameAction::BuyOrb, PullEffect::PointsGteMilestone) => {
+            todo!()
+        }
+        (GameState::Level(level_data), GameAction::BuyOrb, PullEffect::OrbsReachZero) => todo!(),
+        (GameState::Level(level_data), GameAction::NextLevel, PullEffect::HealthReachesZero) => {
+            todo!()
+        }
+        (GameState::Level(level_data), GameAction::NextLevel, PullEffect::PointsLtMilestone) => {
+            todo!()
+        }
+        (GameState::Level(level_data), GameAction::NextLevel, PullEffect::PointsGteMilestone) => {
+            todo!()
+        }
+        (GameState::Level(level_data), GameAction::NextLevel, PullEffect::OrbsReachZero) => todo!(),
+        (
+            GameState::LevelComplete(level_complete_data),
+            GameAction::PullOrb,
+            PullEffect::HealthReachesZero,
+        ) => todo!(),
+        (
+            GameState::LevelComplete(level_complete_data),
+            GameAction::PullOrb,
+            PullEffect::PointsLtMilestone,
+        ) => todo!(),
+        (
+            GameState::LevelComplete(level_complete_data),
+            GameAction::PullOrb,
+            PullEffect::PointsGteMilestone,
+        ) => todo!(),
+        (
+            GameState::LevelComplete(level_complete_data),
+            GameAction::PullOrb,
+            PullEffect::OrbsReachZero,
+        ) => todo!(),
+        (
+            GameState::LevelComplete(level_complete_data),
+            GameAction::CashOut,
+            PullEffect::HealthReachesZero,
+        ) => todo!(),
+        (
+            GameState::LevelComplete(level_complete_data),
+            GameAction::CashOut,
+            PullEffect::PointsLtMilestone,
+        ) => todo!(),
+        (
+            GameState::LevelComplete(level_complete_data),
+            GameAction::CashOut,
+            PullEffect::PointsGteMilestone,
+        ) => todo!(),
+        (
+            GameState::LevelComplete(level_complete_data),
+            GameAction::CashOut,
+            PullEffect::OrbsReachZero,
+        ) => todo!(),
+        (
+            GameState::LevelComplete(level_complete_data),
+            GameAction::EnterShop,
+            PullEffect::HealthReachesZero,
+        ) => todo!(),
+        (
+            GameState::LevelComplete(level_complete_data),
+            GameAction::EnterShop,
+            PullEffect::PointsLtMilestone,
+        ) => todo!(),
+        (
+            GameState::LevelComplete(level_complete_data),
+            GameAction::EnterShop,
+            PullEffect::PointsGteMilestone,
+        ) => todo!(),
+        (
+            GameState::LevelComplete(level_complete_data),
+            GameAction::EnterShop,
+            PullEffect::OrbsReachZero,
+        ) => todo!(),
+        (
+            GameState::LevelComplete(level_complete_data),
+            GameAction::BuyOrb,
+            PullEffect::HealthReachesZero,
+        ) => todo!(),
+        (
+            GameState::LevelComplete(level_complete_data),
+            GameAction::BuyOrb,
+            PullEffect::PointsLtMilestone,
+        ) => todo!(),
+        (
+            GameState::LevelComplete(level_complete_data),
+            GameAction::BuyOrb,
+            PullEffect::PointsGteMilestone,
+        ) => todo!(),
+        (
+            GameState::LevelComplete(level_complete_data),
+            GameAction::BuyOrb,
+            PullEffect::OrbsReachZero,
+        ) => todo!(),
+        (
+            GameState::LevelComplete(level_complete_data),
+            GameAction::NextLevel,
+            PullEffect::HealthReachesZero,
+        ) => todo!(),
+        (
+            GameState::LevelComplete(level_complete_data),
+            GameAction::NextLevel,
+            PullEffect::PointsLtMilestone,
+        ) => todo!(),
+        (
+            GameState::LevelComplete(level_complete_data),
+            GameAction::NextLevel,
+            PullEffect::PointsGteMilestone,
+        ) => todo!(),
+        (
+            GameState::LevelComplete(level_complete_data),
+            GameAction::NextLevel,
+            PullEffect::OrbsReachZero,
+        ) => todo!(),
+        (GameState::Shop(shop_data), GameAction::PullOrb, PullEffect::HealthReachesZero) => todo!(),
+        (GameState::Shop(shop_data), GameAction::PullOrb, PullEffect::PointsLtMilestone) => todo!(),
+        (GameState::Shop(shop_data), GameAction::PullOrb, PullEffect::PointsGteMilestone) => {
+            todo!()
+        }
+        (GameState::Shop(shop_data), GameAction::PullOrb, PullEffect::OrbsReachZero) => todo!(),
+        (GameState::Shop(shop_data), GameAction::CashOut, PullEffect::HealthReachesZero) => todo!(),
+        (GameState::Shop(shop_data), GameAction::CashOut, PullEffect::PointsLtMilestone) => todo!(),
+        (GameState::Shop(shop_data), GameAction::CashOut, PullEffect::PointsGteMilestone) => {
+            todo!()
+        }
+        (GameState::Shop(shop_data), GameAction::CashOut, PullEffect::OrbsReachZero) => todo!(),
+        (GameState::Shop(shop_data), GameAction::EnterShop, PullEffect::HealthReachesZero) => {
+            todo!()
+        }
+        (GameState::Shop(shop_data), GameAction::EnterShop, PullEffect::PointsLtMilestone) => {
+            todo!()
+        }
+        (GameState::Shop(shop_data), GameAction::EnterShop, PullEffect::PointsGteMilestone) => {
+            todo!()
+        }
+        (GameState::Shop(shop_data), GameAction::EnterShop, PullEffect::OrbsReachZero) => todo!(),
+        (GameState::Shop(shop_data), GameAction::BuyOrb, PullEffect::HealthReachesZero) => todo!(),
+        (GameState::Shop(shop_data), GameAction::BuyOrb, PullEffect::PointsLtMilestone) => todo!(),
+        (GameState::Shop(shop_data), GameAction::BuyOrb, PullEffect::PointsGteMilestone) => todo!(),
+        (GameState::Shop(shop_data), GameAction::BuyOrb, PullEffect::OrbsReachZero) => todo!(),
+        (GameState::Shop(shop_data), GameAction::NextLevel, PullEffect::HealthReachesZero) => {
+            todo!()
+        }
+        (GameState::Shop(shop_data), GameAction::NextLevel, PullEffect::PointsLtMilestone) => {
+            todo!()
+        }
+        (GameState::Shop(shop_data), GameAction::NextLevel, PullEffect::PointsGteMilestone) => {
+            todo!()
+        }
+        (GameState::Shop(shop_data), GameAction::NextLevel, PullEffect::OrbsReachZero) => todo!(),
+        (
+            GameState::CashedOut(cashed_out_data),
+            GameAction::PullOrb,
+            PullEffect::HealthReachesZero,
+        ) => todo!(),
+        (
+            GameState::CashedOut(cashed_out_data),
+            GameAction::PullOrb,
+            PullEffect::PointsLtMilestone,
+        ) => todo!(),
+        (
+            GameState::CashedOut(cashed_out_data),
+            GameAction::PullOrb,
+            PullEffect::PointsGteMilestone,
+        ) => todo!(),
+        (GameState::CashedOut(cashed_out_data), GameAction::PullOrb, PullEffect::OrbsReachZero) => {
+            todo!()
+        }
+        (
+            GameState::CashedOut(cashed_out_data),
+            GameAction::CashOut,
+            PullEffect::HealthReachesZero,
+        ) => todo!(),
+        (
+            GameState::CashedOut(cashed_out_data),
+            GameAction::CashOut,
+            PullEffect::PointsLtMilestone,
+        ) => todo!(),
+        (
+            GameState::CashedOut(cashed_out_data),
+            GameAction::CashOut,
+            PullEffect::PointsGteMilestone,
+        ) => todo!(),
+        (GameState::CashedOut(cashed_out_data), GameAction::CashOut, PullEffect::OrbsReachZero) => {
+            todo!()
+        }
+        (
+            GameState::CashedOut(cashed_out_data),
+            GameAction::EnterShop,
+            PullEffect::HealthReachesZero,
+        ) => todo!(),
+        (
+            GameState::CashedOut(cashed_out_data),
+            GameAction::EnterShop,
+            PullEffect::PointsLtMilestone,
+        ) => todo!(),
+        (
+            GameState::CashedOut(cashed_out_data),
+            GameAction::EnterShop,
+            PullEffect::PointsGteMilestone,
+        ) => todo!(),
+        (
+            GameState::CashedOut(cashed_out_data),
+            GameAction::EnterShop,
+            PullEffect::OrbsReachZero,
+        ) => todo!(),
+        (
+            GameState::CashedOut(cashed_out_data),
+            GameAction::BuyOrb,
+            PullEffect::HealthReachesZero,
+        ) => todo!(),
+        (
+            GameState::CashedOut(cashed_out_data),
+            GameAction::BuyOrb,
+            PullEffect::PointsLtMilestone,
+        ) => todo!(),
+        (
+            GameState::CashedOut(cashed_out_data),
+            GameAction::BuyOrb,
+            PullEffect::PointsGteMilestone,
+        ) => todo!(),
+        (GameState::CashedOut(cashed_out_data), GameAction::BuyOrb, PullEffect::OrbsReachZero) => {
+            todo!()
+        }
+        (
+            GameState::CashedOut(cashed_out_data),
+            GameAction::NextLevel,
+            PullEffect::HealthReachesZero,
+        ) => todo!(),
+        (
+            GameState::CashedOut(cashed_out_data),
+            GameAction::NextLevel,
+            PullEffect::PointsLtMilestone,
+        ) => todo!(),
+        (
+            GameState::CashedOut(cashed_out_data),
+            GameAction::NextLevel,
+            PullEffect::PointsGteMilestone,
+        ) => todo!(),
+        (
+            GameState::CashedOut(cashed_out_data),
+            GameAction::NextLevel,
+            PullEffect::OrbsReachZero,
+        ) => todo!(),
+        (
+            GameState::GameOver(game_over_data),
+            GameAction::PullOrb,
+            PullEffect::HealthReachesZero,
+        ) => todo!(),
+        (
+            GameState::GameOver(game_over_data),
+            GameAction::PullOrb,
+            PullEffect::PointsLtMilestone,
+        ) => todo!(),
+        (
+            GameState::GameOver(game_over_data),
+            GameAction::PullOrb,
+            PullEffect::PointsGteMilestone,
+        ) => todo!(),
+        (GameState::GameOver(game_over_data), GameAction::PullOrb, PullEffect::OrbsReachZero) => {
+            todo!()
+        }
+        (
+            GameState::GameOver(game_over_data),
+            GameAction::CashOut,
+            PullEffect::HealthReachesZero,
+        ) => todo!(),
+        (
+            GameState::GameOver(game_over_data),
+            GameAction::CashOut,
+            PullEffect::PointsLtMilestone,
+        ) => todo!(),
+        (
+            GameState::GameOver(game_over_data),
+            GameAction::CashOut,
+            PullEffect::PointsGteMilestone,
+        ) => todo!(),
+        (GameState::GameOver(game_over_data), GameAction::CashOut, PullEffect::OrbsReachZero) => {
+            todo!()
+        }
+        (
+            GameState::GameOver(game_over_data),
+            GameAction::EnterShop,
+            PullEffect::HealthReachesZero,
+        ) => todo!(),
+        (
+            GameState::GameOver(game_over_data),
+            GameAction::EnterShop,
+            PullEffect::PointsLtMilestone,
+        ) => todo!(),
+        (
+            GameState::GameOver(game_over_data),
+            GameAction::EnterShop,
+            PullEffect::PointsGteMilestone,
+        ) => todo!(),
+        (GameState::GameOver(game_over_data), GameAction::EnterShop, PullEffect::OrbsReachZero) => {
+            todo!()
+        }
+        (
+            GameState::GameOver(game_over_data),
+            GameAction::BuyOrb,
+            PullEffect::HealthReachesZero,
+        ) => todo!(),
+        (
+            GameState::GameOver(game_over_data),
+            GameAction::BuyOrb,
+            PullEffect::PointsLtMilestone,
+        ) => todo!(),
+        (
+            GameState::GameOver(game_over_data),
+            GameAction::BuyOrb,
+            PullEffect::PointsGteMilestone,
+        ) => todo!(),
+        (GameState::GameOver(game_over_data), GameAction::BuyOrb, PullEffect::OrbsReachZero) => {
+            todo!()
+        }
+        (
+            GameState::GameOver(game_over_data),
+            GameAction::NextLevel,
+            PullEffect::HealthReachesZero,
+        ) => todo!(),
+        (
+            GameState::GameOver(game_over_data),
+            GameAction::NextLevel,
+            PullEffect::PointsLtMilestone,
+        ) => todo!(),
+        (
+            GameState::GameOver(game_over_data),
+            GameAction::NextLevel,
+            PullEffect::PointsGteMilestone,
+        ) => todo!(),
+        (GameState::GameOver(game_over_data), GameAction::NextLevel, PullEffect::OrbsReachZero) => {
+            todo!()
+        }
     }
 }
