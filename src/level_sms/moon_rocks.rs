19abd9199c50896efef4da560339a7c00b0b6bb6
elton-cs <govada.elton@gmail.com>
2025-11-21 17:05:55 -0600

    smimpl: moon_rocks

--- src/level_sms/moon_rocks.rs
+++ src/level_sms/moon_rocks.rs
@@ -1,27 +1,40 @@
-type MoonRocks = Option<MoonRocksData>;
+use crate::invalid_states::InvalidState;
 
-struct MoonRocksData;
-
-enum MoonRocksAction {
-    Earn,
-    Spend,
+enum MoonRocks {
+    Zero,
+    Some(u32),
 }
 
-enum MoonRocksEffect {
-    SomeMR,
-    ZeroMR,
+enum MoonRocksAction {
+    Earn(u32),
+    Spend(u32),
 }
 
 fn stf_moon_rocks(
     moon_rocks: MoonRocks,
     action: &MoonRocksAction,
-    effect: &MoonRocksEffect,
-) -> MoonRocks {
-    match (moon_rocks, action, effect) {
-        (None, MoonRocksAction::Earn, _) => Some(MoonRocksData),
-        (Some(_), MoonRocksAction::Earn, _) => Some(MoonRocksData),
-        (Some(_), MoonRocksAction::Spend, MoonRocksEffect::SomeMR) => Some(MoonRocksData),
-        (Some(_), MoonRocksAction::Spend, MoonRocksEffect::ZeroMR) => None,
-        (state, _, _) => state,
+) -> Result<MoonRocks, InvalidState> {
+    match (moon_rocks, action) {
+        (MoonRocks::Zero, MoonRocksAction::Earn(amount)) => match amount {
+            0 => Err(InvalidState::InvalidMoonRocksCannotEarnZero),
+            n => Ok(MoonRocks::Some(*n)),
+        },
+        (MoonRocks::Some(current), MoonRocksAction::Earn(amount)) => match amount {
+            0 => Err(InvalidState::InvalidMoonRocksCannotEarnZero),
+            n => Ok(MoonRocks::Some(current + n)),
+        },
+        (MoonRocks::Some(current), MoonRocksAction::Spend(amount)) => match amount {
+            0 => Err(InvalidState::InvalidMoonRocksCannotSpendZero),
+            n if *n > current => Err(InvalidState::InvalidMoonRocks),
+            n if current - n == 0 => Ok(MoonRocks::Zero),
+            n => Ok(MoonRocks::Some(current - n)),
+        },
+        (_, _) => Err(InvalidState::InvalidMoonRocks),
+    }
+}
+
+impl MoonRocks {
+    fn new() -> Self {
+        MoonRocks::Zero
     }
 }
