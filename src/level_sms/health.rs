31d6b92b5f521e25c6855a341d37859604f751ec
elton-cs <govada.elton@gmail.com>
2025-11-21 17:10:06 -0600

    smimpl: health

--- src/level_sms/health.rs
+++ src/level_sms/health.rs
@@ -1,37 +1,41 @@
-enum Health {
-    Max(FullData),
-    Damaged(DamagedData),
-    Zero(ZeroData),
-}
+use crate::invalid_states::InvalidState;
 
-struct FullData;
-struct DamagedData;
-struct ZeroData;
+const MAX_HEALTH: u32 = 100;
+
+enum Health {
+    Zero,
+    Damaged(u32),
+    Max,
+}
 
 enum HealthAction {
-    Heal,
-    TakeDamage,
+    Heal(u32),
+    TakeDamage(u32),
 }
 
-enum HealthEffect {
-    MaxHP,
-    SomeHP,
-    ZeroHP,
+fn stf_health(health: Health, action: &HealthAction) -> Result<Health, InvalidState> {
+    match (health, action) {
+        (Health::Max, HealthAction::TakeDamage(dmg)) => match dmg {
+            0 => Err(InvalidState::InvalidHealth),
+            n if *n >= MAX_HEALTH => Ok(Health::Zero),
+            n => Ok(Health::Damaged(MAX_HEALTH - n)),
+        },
+        (Health::Damaged(hp), HealthAction::Heal(heal)) => match heal {
+            0 => Err(InvalidState::InvalidHealth),
+            n if hp + n >= MAX_HEALTH => Ok(Health::Max),
+            n => Ok(Health::Damaged(hp + n)),
+        },
+        (Health::Damaged(hp), HealthAction::TakeDamage(dmg)) => match dmg {
+            0 => Err(InvalidState::InvalidHealth),
+            n if *n >= hp => Ok(Health::Zero),
+            n => Ok(Health::Damaged(hp - n)),
+        },
+        (_, _) => Err(InvalidState::InvalidHealth),
+    }
 }
 
-fn stf_health(health: Health, action: &HealthAction, effect: &HealthEffect) -> Health {
-    match (health, action, effect) {
-        (Health::Max(_), HealthAction::TakeDamage, _) => Health::Damaged(DamagedData),
-        (Health::Damaged(_), HealthAction::Heal, HealthEffect::MaxHP) => Health::Max(FullData),
-        (Health::Damaged(_), HealthAction::Heal, HealthEffect::SomeHP) => {
-            Health::Damaged(DamagedData)
-        }
-        (Health::Damaged(_), HealthAction::TakeDamage, HealthEffect::SomeHP) => {
-            Health::Damaged(DamagedData)
-        }
-        (Health::Damaged(_), HealthAction::TakeDamage, HealthEffect::ZeroHP) => {
-            Health::Zero(ZeroData)
-        }
-        (state, _, _) => state,
+impl Health {
+    fn new() -> Self {
+        Health::Max
     }
 }
