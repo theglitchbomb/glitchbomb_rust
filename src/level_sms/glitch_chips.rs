2ece282564e9b8d45051d152aa1b018705a1384e
elton-cs <govada.elton@gmail.com>
2025-11-21 16:38:38 -0600

    smimpl: glitch_chips

--- src/level_sms/glitch_chips.rs
+++ src/level_sms/glitch_chips.rs
@@ -1,27 +1,40 @@
-type GlitchChips = Option<GlitchChipsData>;
+use crate::invalid_states::InvalidState;
 
-struct GlitchChipsData;
-
-enum GlitchChipsAction {
-    Earn,
-    Spend,
+enum GlitchChips {
+    Zero,
+    Some(u32),
 }
 
-enum GlitchChipsEffect {
-    SomeGC,
-    ZeroGC,
+enum GlitchChipsAction {
+    Earn(u32),
+    Spend(u32),
 }
 
 fn stf_glitch_chips(
     glitch_chips: GlitchChips,
     action: &GlitchChipsAction,
-    effect: &GlitchChipsEffect,
-) -> GlitchChips {
-    match (glitch_chips, action, effect) {
-        (None, GlitchChipsAction::Earn, _) => Some(GlitchChipsData),
-        (Some(_), GlitchChipsAction::Earn, _) => Some(GlitchChipsData),
-        (Some(_), GlitchChipsAction::Spend, GlitchChipsEffect::SomeGC) => Some(GlitchChipsData),
-        (Some(_), GlitchChipsAction::Spend, GlitchChipsEffect::ZeroGC) => None,
-        (state, _, _) => state,
+) -> Result<GlitchChips, InvalidState> {
+    match (glitch_chips, action) {
+        (GlitchChips::Zero, GlitchChipsAction::Earn(amount)) => match amount {
+            0 => Err(InvalidState::InvalidGlitchChipsCannotEarnZero),
+            n => Ok(GlitchChips::Some(*n)),
+        },
+        (GlitchChips::Some(current), GlitchChipsAction::Earn(amount)) => match amount {
+            0 => Err(InvalidState::InvalidGlitchChipsCannotEarnZero),
+            n => Ok(GlitchChips::Some(current + n)),
+        },
+        (GlitchChips::Some(current), GlitchChipsAction::Spend(amount)) => match amount {
+            0 => Err(InvalidState::InvalidGlitchChipsCannotSpendZero),
+            n if *n > current => Err(InvalidState::InvalidGlitchChips),
+            n if current - n == 0 => Ok(GlitchChips::Zero),
+            n => Ok(GlitchChips::Some(current - n)),
+        },
+        (_, _) => Err(InvalidState::InvalidGlitchChips),
+    }
+}
+
+impl GlitchChips {
+    fn new() -> Self {
+        GlitchChips::Zero
     }
 }
