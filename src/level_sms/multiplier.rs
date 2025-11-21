9c9573cc44ef0047e36b50ac1e97548e99962638
elton-cs <govada.elton@gmail.com>
2025-11-21 17:18:54 -0600

    smimpl: multiplier

--- src/level_sms/multiplier.rs
+++ src/level_sms/multiplier.rs
@@ -1,19 +1,42 @@
-enum Multiplier {
-    Base(BaseData),
-    Factor(FactorData),
-}
+use crate::invalid_states::InvalidState;
 
-struct BaseData;
-struct FactorData;
+const BASE_MULTIPLIER: u32 = 100;
+const MULTIPLIER_INCREMENT: u32 = 50;
+
+enum Multiplier {
+    Base,
+    Factor(u32),
+}
 
 enum MultiplierAction {
-    AddMultiplier,
+    Add(u32),
 }
 
-fn stf_multiplier(multiplier: Multiplier, action: &MultiplierAction) -> Multiplier {
+fn stf_multiplier(
+    multiplier: Multiplier,
+    action: &MultiplierAction,
+) -> Result<Multiplier, InvalidState> {
     match (multiplier, action) {
-        (Multiplier::Base(_), MultiplierAction::AddMultiplier) => Multiplier::Factor(FactorData),
-        (Multiplier::Factor(_), MultiplierAction::AddMultiplier) => Multiplier::Factor(FactorData),
-        (state, _) => state,
+        (Multiplier::Base, MultiplierAction::Add(amount)) => match amount {
+            0 => Err(InvalidState::InvalidMultiplierCannotAddZero),
+            n if n % MULTIPLIER_INCREMENT != 0 => {
+                Err(InvalidState::InvalidMultiplierMustBeMultipleOf50)
+            }
+            n => Ok(Multiplier::Factor(BASE_MULTIPLIER + n)),
+        },
+        (Multiplier::Factor(current), MultiplierAction::Add(amount)) => match amount {
+            0 => Err(InvalidState::InvalidMultiplierCannotAddZero),
+            n if n % MULTIPLIER_INCREMENT != 0 => {
+                Err(InvalidState::InvalidMultiplierMustBeMultipleOf50)
+            }
+            n => Ok(Multiplier::Factor(current + n)),
+        },
+        (_, _) => Err(InvalidState::InvalidMultiplier),
+    }
+}
+
+impl Multiplier {
+    fn new() -> Self {
+        Multiplier::Base
     }
 }
