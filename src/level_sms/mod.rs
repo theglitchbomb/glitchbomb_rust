582b89615a405dc9e29415fc005e8b79506d80cd
elton-cs <govada.elton@gmail.com>
2025-11-21 18:05:37 -0600

    smimpl: orb matrix v1

--- src/level_sms/mod.rs
+++ src/level_sms/mod.rs
@@ -1,7 +1,7 @@
-pub mod bag;
 pub mod glitch_chips;
 pub mod health;
 pub mod level;
 pub mod moon_rocks;
 pub mod multiplier;
+pub mod orb_matrix;
 pub mod points;
---
+++ src/level_sms/orb_matrix.rs
@@ -0,0 +1,77 @@
+use crate::invalid_states::InvalidState;
+
+#[derive(Copy, Clone)]
+enum OrbEffect {
+    Health(u32),
+    Points(u32),
+    Bomb(u32),
+    PointsPerAnyOrb(u32),
+    PointsPerBombPulled(u32),
+    Multiplier(u32),
+    GlitchChips(u32),
+    MoonRocks(u32),
+    // PointRewind,
+    // BombImmunity,
+    // RiskFiveOrDie,
+}
+
+struct Internals {
+    pullable: Vec<OrbEffect>,
+    consumed: Vec<OrbEffect>,
+}
+
+enum OrbMatrix {
+    Full(Internals),
+    InProgress(Internals),
+    Empty,
+}
+
+enum OrbMatrixAction {
+    Pull,
+}
+
+fn stf_orb_matrix(
+    orb_matrix: OrbMatrix,
+    action: &OrbMatrixAction,
+) -> Result<(OrbMatrix, OrbEffect), InvalidState> {
+    match (orb_matrix, action) {
+        (OrbMatrix::Full(mut internals), OrbMatrixAction::Pull) => match internals.pullable.len() {
+            0 => Err(InvalidState::InvalidBag),
+            _ => {
+                let pulled_orb = internals.pullable.pop().unwrap();
+                internals.consumed.push(pulled_orb);
+                Ok((OrbMatrix::InProgress(internals), pulled_orb))
+            }
+        },
+        (OrbMatrix::InProgress(mut internals), OrbMatrixAction::Pull) => {
+            match internals.pullable.len() {
+                0 => Err(InvalidState::InvalidBag),
+                _ => {
+                    let orb = internals.pullable.pop().unwrap();
+                    internals.consumed.push(orb);
+                    Ok((OrbMatrix::InProgress(internals), orb))
+                }
+            }
+        }
+        (_, _) => Err(InvalidState::InvalidBag),
+    }
+}
+
+impl OrbMatrix {
+    fn new() -> Self {
+        let pullable = vec![
+            OrbEffect::Health(0),
+            OrbEffect::Points(0),
+            OrbEffect::Multiplier(0),
+            OrbEffect::Bomb(0),
+            OrbEffect::GlitchChips(0),
+            OrbEffect::MoonRocks(0),
+            OrbEffect::Health(0),
+            OrbEffect::Points(0),
+            OrbEffect::Multiplier(0),
+            OrbEffect::Bomb(0),
+        ];
+        let consumed = Vec::new();
+        OrbMatrix::Full(Internals { pullable, consumed })
+    }
+}
